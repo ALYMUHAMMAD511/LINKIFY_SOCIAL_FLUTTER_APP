7c13053d4d927d4265ba9278de6d3832e8e4b337
alymu <alym7mdd@gmail.com>
2023-04-25 00:50:13 +0200

    modify

--- lib/modules/login/login_screen.dart
+++ lib/modules/login/login_screen.dart
@@ -1,7 +1,9 @@
 import 'package:conditional_builder_null_safety/conditional_builder_null_safety.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:social_app/modules/login/login_cubit/cubit.dart';
 import 'package:social_app/modules/login/login_cubit/states.dart';
+import 'package:social_app/modules/register/register_screen.dart';
 import 'package:social_app/shared/components/components.dart';
 import 'package:toast/toast.dart';
 
@@ -16,120 +18,125 @@ class LoginScreen extends StatelessWidget {
   Widget build(BuildContext context)
   {
     ToastContext().init(context);
-    return Scaffold(
-      appBar: AppBar(),
-      body: Center(
-        child: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Form(
-              key: formKey,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children:
-                [
-                  Text(
-                    'LOGIN',
-                    style: Theme.of(context).textTheme.headlineMedium!.copyWith(
-                      color: Colors.black,
-                    ),
-                  ),
-                  Text(
-                    'Login now to browse our hot offers',
-                    style: Theme.of(context).textTheme.bodyLarge?.copyWith(
-                        color: Colors.grey
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 30.0,
-                  ),
-                  defaultFormField(
-                    controller: emailController,
-                    type: TextInputType.emailAddress,
-                    validate: (String? value)
-                    {
-                      if (value!.isEmpty)
-                      {
-                        return 'Please, Enter your Email Address';
-                      }
-                      return null;
-                    },
-                    labelText: 'Email Address',
-                    prefixIcon: Icons.email_outlined,
-                  ),
-                  const SizedBox(
-                    height: 17.0,
-                  ),
-                  defaultFormField(
-                    controller: passwordController,
-                    type: TextInputType.visiblePassword,
-                    suffixIcon: LoginCubit.get(context).suffix,
-                    onFieldSubmitted: (value)
-                    {
-                      if (formKey.currentState!.validate())
-                      {
-                        LoginCubit.get(context).userLogin(
-                          email: emailController.text,
-                          password: passwordController.text,
-                        );
-                      }
-                    },
-                    isPassword: LoginCubit.get(context).isPasswordShown,
-                    suffixPressed: ()
-                    {
-                      LoginCubit.get(context).changePasswordVisibility();
-                    },
-                    validate: (value)
-                    {
-                      if (value!.isEmpty)
-                      {
-                        return 'Password should not be Empty';
-                      }
-                      return null;
-                    },
-                    labelText: 'Password',
-                    prefixIcon: Icons.lock_outline,
-                  ),
-                  const SizedBox(
-                    height: 20.0,
-                  ),
-                  ConditionalBuilder(
-                    condition: state is! LoginLoadingState,
-                    builder: (context) => defaultButton(
-                      onPressed: ()
-                      {
-                        if (formKey.currentState!.validate())
-                        {
-                          LoginCubit.get(context).userLogin(
-                              email: emailController.text,
-                              password: passwordController.text
-                          );
-                        }
-                      },
-                      text: 'login',
-                      isUpperCase: true,
-                    ),
-                    fallback: (context) => const Center(child: CircularProgressIndicator()),
-                  ),
-                  const SizedBox(
-                    height: 17.0,
-                  ),
-                  Row(
+    return BlocProvider(
+      create: (BuildContext context) => LoginCubit(),
+      child: BlocConsumer <LoginCubit, LoginStates>(
+        listener: (context, state) {},
+        builder:(context, state) => Scaffold(
+          appBar: AppBar(),
+          body: Center(
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Form(
+                  key: formKey,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children:
                     [
-                      const Text(
-                        'Don\'t have an Account?',
+                      Text(
+                        'LOGIN',
+                        style: Theme.of(context).textTheme.headlineMedium!.copyWith(
+                          color: Colors.black,
+                        ),
                       ),
-                      defaultTextButton(
-                        onPressed: ()
+                      Text(
+                        'Login now to browse our hot offers',
+                        style: Theme.of(context).textTheme.bodyLarge?.copyWith(
+                            color: Colors.grey
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 30.0,
+                      ),
+                      defaultFormField(
+                        controller: emailController,
+                        type: TextInputType.emailAddress,
+                        validate: (String? value)
                         {
-                          navigateTo(context, RegisterScreen());
+                          if (value!.isEmpty)
+                          {
+                            return 'Please, Enter your Email Address';
+                          }
+                          return null;
                         },
-                        text: 'register',
+                        labelText: 'Email Address',
+                        prefixIcon: Icons.email_outlined,
+                      ),
+                      const SizedBox(
+                        height: 17.0,
+                      ),
+                      defaultFormField(
+                        controller: passwordController,
+                        type: TextInputType.visiblePassword,
+                        suffixIcon: LoginCubit.get(context).suffix,
+                        onFieldSubmitted: (value)
+                        {
+                          // if (formKey.currentState!.validate())
+                          // {
+                          //   LoginCubit.get(context).userLogin(
+                          //     email: emailController.text,
+                          //     password: passwordController.text,
+                          //   );
+                          // }
+                        },
+                        isPassword: LoginCubit.get(context).isPasswordShown,
+                        suffixPressed: ()
+                        {
+                          LoginCubit.get(context).changePasswordVisibility();
+                        },
+                        validate: (value)
+                        {
+                          if (value!.isEmpty)
+                          {
+                            return 'Password should not be Empty';
+                          }
+                          return null;
+                        },
+                        labelText: 'Password',
+                        prefixIcon: Icons.lock_outline,
+                      ),
+                      const SizedBox(
+                        height: 20.0,
+                      ),
+                      ConditionalBuilder(
+                        condition: state is! LoginLoadingState,
+                        builder: (context) => defaultButton(
+                          onPressed: ()
+                          {
+                            // if (formKey.currentState!.validate())
+                            // {
+                            //   LoginCubit.get(context).userLogin(
+                            //       email: emailController.text,
+                            //       password: passwordController.text
+                            //   );
+                            },
+                          text: 'login',
+                          isUpperCase: true,
+                        ),
+                        fallback: (context) => const Center(child: CircularProgressIndicator()),
+                      ),
+                      const SizedBox(
+                        height: 17.0,
+                      ),
+                      Row(
+                        children:
+                        [
+                          const Text(
+                            'Don\'t have an Account?',
+                          ),
+                          defaultTextButton(
+                            onPressed: ()
+                            {
+                              navigateTo(context, RegisterScreen());
+                            },
+                            text: 'register',
+                          ),
+                        ],
                       ),
                     ],
                   ),
-                ],
+                ),
               ),
             ),
           ),
---
+++ lib/modules/register/register_cubit/cubit.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:social_app/modules/register/register_cubit/states.dart';
+
+
+class RegisterCubit extends Cubit <RegisterStates> {
+  RegisterCubit() : super(RegisterInitialState());
+
+  static RegisterCubit get(context) => BlocProvider.of(context);
+  // late LoginModel loginModel;
+  //
+  // void userRegister({
+  //   required String email,
+  //   required String password,
+  //   required String name,
+  //   required String phone,
+  // }) {
+  //   emit(RegisterLoadingState());
+  //   DioHelper.postData(
+  //       url: REGISTER,
+  //       data: {
+  //         'email': email,
+  //         'password': password,
+  //         'name' : name,
+  //         'phone': phone,
+  //       }).then((value) {
+  //     if (kDebugMode) {
+  //       print(value.data);
+  //       loginModel = LoginModel.fromJson(value.data);
+  //       emit(RegisterSuccessState(loginModel));
+  //     }
+  //   }).catchError((error) {
+  //     if (kDebugMode) {
+  //       print(error.toString());
+  //     }
+  //     emit(RegisterErrorState(error));
+  //   });
+  // }
+
+  IconData suffix = Icons.visibility_outlined;
+  bool isPasswordShown = true;
+
+  void changePasswordVisibility() {
+    isPasswordShown = !isPasswordShown;
+    suffix =
+    isPasswordShown ? Icons.visibility_outlined : Icons.visibility_off_outlined;
+    emit(ChangeRegisterPasswordVisibilityState());
+  }
+}
